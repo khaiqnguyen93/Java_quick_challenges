3b4108c285d92999411a894df7a3195699aca7fa
khaiqnguyen93 <khaiqnguyen93@gmail.com>
2020-03-10 22:49:05 +0700

    1. Implement sing() for bird a - add unit test for Bird

---
+++ src/main/java/assignment/code/Solution.java
@@ -0,0 +1,10 @@
+package assignment.code;
+
+public class Solution {
+	public static void main(String[] args) {
+		Bird bird = new Bird();
+		bird.walk();
+		bird.fly();
+		bird.sing();
+	}
+}
---
+++ src/test/java/assignment2359/BirdTest.java
@@ -0,0 +1,50 @@
+package assignment2359;
+
+import static org.hamcrest.core.StringContains.containsString;
+import static org.junit.Assert.assertThat;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import assignment.code.Bird;
+
+public class BirdTest {
+	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+	private final PrintStream originalOut = System.out;
+
+	@Before
+	public void setUpStreams() {
+	    System.setOut(new PrintStream(outContent));
+	}
+
+	@After
+	public void restoreStreams() {
+	    System.setOut(originalOut);
+	}
+
+	@Test
+	public void testWalk() {
+	    Bird bird = new Bird();
+	    bird.walk();
+	    assertThat(outContent.toString(), containsString("I am walking"));
+	}
+
+	@Test
+	public void testFly() {
+	    Bird bird = new Bird();
+	    bird.fly();
+	    assertThat(outContent.toString(), containsString("I am flying"));
+	}
+
+	@Test
+	public void testSing() {
+	    Bird bird = new Bird();
+	    bird.sing();
+	    assertThat(outContent.toString(), containsString("I am singing"));
+	}
+
+}
